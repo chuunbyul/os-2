ccd8a6ca133c73b82aa6646ddce863aac3a4add4
chuunbyul <roehd2001@gmail.com>
2024-06-03 15:20:07 +0900

    파일 이름 변경 commands.txt 변경

--- os과제2/complete version.cpp
+++ os과제2/complete version.cpp
@@ -1,402 +1,287 @@
-//// 2-2, 2-3
-//// 2-2는 정확하지 않음
-//// 2-3은 gpt 없이 70% 구현
-////(-m 구현 못함)
-//// parse()는 vector로 구현
-//#include <iostream>
-//#include <fstream>
-//#include <sstream>
-//#include <string>
-//#include <vector>
-//#include <chrono>
-//#include <thread>
-//#include <cstdlib>
-//#include <Windows.h>
-//#include <list>
-//#include <memory>
-//#include <mutex>
-//#include <atomic>
-//#include <condition_variable>
-//#include <map>
-//#include <algorithm>
-//#include <sstream>
-//
-//#define Y   500
-//using namespace std;
-//
-//class J {
-//public:
-//    int i;
-//    bool f;
-//    string c;
-//    bool p = false;
-//    int t = 0;
-//
-//    J(int i, bool f, const string& c)
-//        : i(i), f(f), c(c) {}
-//
-//    string fmt() const {
-//        stringstream ss;
-//        ss << "[" << i << (f ? "F" : "B");
-//        if (p) ss << "*";
-//        ss << "]";
-//        return ss.str();
-//    }
-//
-//    string fmtTQ() const {
-//        stringstream ss;
-//        ss << i << (f ? "F" : "B") << ":" << t;
-//        return ss.str();
-//    }
-//};
-//
-//class JQ {
-//private:
-//    mutex m;
-//    condition_variable cv;
-//    list<shared_ptr<J>> fj;
-//    list<shared_ptr<J>> bj;
-//    map<int, shared_ptr<J>> tq;
-//    atomic<int> bc{ 0 };
-//
-//public:
-//    atomic<int> tc{ 0 };
-//    void addJ(shared_ptr<J> j) {
-//        lock_guard<mutex> lock(m);
-//        if (j->f) {
-//            fj.push_back(j);
-//        }
-//        else {
-//            bj.push_back(j);
-//            bc++;
-//        }
-//        tc++;
-//    }
-//
-//    void sleep(int i, int s) {
-//        lock_guard<mutex> lock(m);
-//        auto it = find_if(fj.begin(), fj.end(), [&i](const auto& j) { return j->i == i; });
-//        if (it != fj.end()) {
-//            tq[i] = *it;
-//            fj.erase(it);
-//        }
-//        else {
-//            it = find_if(bj.begin(), bj.end(), [&i](const auto& j) { return j->i == i; });
-//            if (it != bj.end()) {
-//                tq[i] = *it;
-//                bj.erase(it);
-//                bc--;
-//            }
-//        }
-//        tq[i]->t = s;
-//    }
-//
-//    void wake() {
-//        lock_guard<mutex> lock(m);
-//        for (auto it = tq.begin(); it != tq.end();) {
-//            if (--it->second->t <= 0) {
-//                if (it->second->f) {
-//                    fj.push_back(it->second);
-//                }
-//                else {
-//                    bj.push_back(it->second);
-//                    bc++;
-//                }
-//                it = tq.erase(it);
-//            }
-//            else {
-//                ++it;
-//            }
-//        }
-//    }
-//
-//    void display(int fpc) {
-//        Sleep(Y);
-//        lock_guard<mutex> lock(m);
-//        cout << endl;
-//        cout << "Running: [" << bc.load() << "B] [" << fpc << "F]" << endl;
-//        cout << "---------------------------" << endl;
-//        cout << "DQ: ";
-//        if (!bj.empty()) {
-//            cout << "(bottom) ";
-//            for (const auto& j : bj) {
-//                cout << j->fmt() << " ";
-//            }
-//            cout << endl << "P => ";
-//        }
-//        else {
-//            cout << "(bottom/top)" << endl;
-//        }
-//        if (fj.empty()) {
-//            cout << "[ ";
-//            for (int i = 0; i < fpc; ++i) {
-//                cout << i << "F ";
-//            }
-//            cout << "]";
-//            cout << "(bottom/top)" << endl;
-//        }
-//        else {
-//            cout << "(bottom/top)" << endl;
-//        }
-//        cout << "---------------------------" << endl;
-//        cout << "WQ: [";
-//        for (const auto& wj : tq) {
-//            cout << wj.second->fmtTQ() << " ";
-//        }
-//        cout << "]" << endl << endl;
-//    }
-//};
-//
-//class Semaphore {
-//private:
-//    mutex mtx;
-//    condition_variable cv;
-//    int count;
-//
-//public:
-//    explicit Semaphore(int count_ = 0) : count(count_) {}
-//
-//    void notify() {
-//        unique_lock<mutex> lock(mtx);
-//        count++;
-//        cv.notify_one();
-//    }
-//
-//    void wait() {
-//        unique_lock<mutex> lock(mtx);
-//        while (count == 0) {
-//            cv.wait(lock);
-//        }
-//        count--;
-//    }
-//};
-//
-//void Echo(const vector<string>& t, int r, int p, int d, JQ& jq) {
-//    Sleep(Y);
-//    for (int i = 0; i < r; ++i) {
-//        for (size_t j = 1; j < t.size(); ++j) {
-//            cout << t[j] << " ";
-//        }
-//        cout << endl;
-//
-//        this_thread::sleep_for(chrono::seconds(p));
-//
-//        if (p * r > d) {
-//            exit(0);
-//        }
-//    }
-//}
-//
-//void Dummy(const vector<string>& t, int r, int p, int d, JQ& jq) {
-//    Sleep(Y);
-//    for (int i = 0; i < r; ++i) {
-//        this_thread::sleep_for(chrono::seconds(p));
-//
-//        if (p * r > d) {
-//            exit(0);
-//        }
-//    }
-//}
-//
-//void GCD(const vector<string>& t, int r, int p, int d, JQ& jq) {
-//    Sleep(Y);
-//    for (int i = 0; i < r; ++i) {
-//        if (t.size() != 3) {
-//            cerr << "Invalid arguments for gcd command" << endl;
-//            return;
-//        }
-//
-//        int x = stoi(t[1]);
-//        int y = stoi(t[2]);
-//
-//        while (y != 0) {
-//            int temp = x % y;
-//            x = y;
-//            y = temp;
-//        }
-//
-//        cout << x << endl;
-//
-//        this_thread::sleep_for(chrono::seconds(p));
-//
-//        if (p * r > d) {
-//            exit(0);
-//        }
-//    }
-//}
-//
-//bool isPrime(int n) {
-//    if (n <= 1) return false;
-//    if (n <= 3) return true;
-//    if (n % 2 == 0 || n % 3 == 0) return false;
-//    for (int i = 5; i * i <= n; i += 6) {
-//        if (n % i == 0 || n % (i + 2) == 0) return false;
-//    }
-//    return true;
-//}
-//
-//void Prime(const vector<string>& t, int r, int p, int d, JQ& jq) {
-//    Sleep(Y);
-//    for (int i = 0; i < r; ++i) {
-//        if (t.size() != 2) {
-//            cerr << "Invalid arguments for prime command" << endl;
-//            return;
-//        }
-//
-//        int x = stoi(t[1]);
-//        int pc = 0;
-//
-//        for (int i = 2; i <= x; ++i) {
-//            if (isPrime(i)) {
-//                ++pc;
-//            }
-//        }
-//
-//        cout << pc << endl;
-//
-//        this_thread::sleep_for(chrono::seconds(p));
-//
-//        if (p * r > d) {
-//            exit(0);
-//        }
-//    }
-//}
-//
-//void Sum(const vector<string>& t, int r, int p, int d, JQ& jq) {
-//    Sleep(Y);
-//    for (int i = 0; i < r; ++i) {
-//        if (t.size() != 2) {
-//            cerr << "Invalid arguments for sum command" << endl;
-//            return;
-//        }
-//
-//        int x = stoi(t[1]);
-//        long long s = 0;
-//        const int MOD = 1000000;
-//
-//        for (int i = 1; i <= x; ++i) {
-//            s = (s + i) % MOD;
-//        }
-//
-//        cout << s << endl;
-//
-//        this_thread::sleep_for(chrono::seconds(p));
-//
-//        if (p * r > d) {
-//            exit(0);
-//        }
-//    }
-//}
-//
-//void procFile(const string& filename, int& fpc, JQ& jq) {
-//    ifstream file(filename);
-//    string line;
-//
-//    Semaphore fg_sem(1); // ForeGround semaphore
-//
-//    while (getline(file, line)) {
-//        istringstream iss(line);
-//        vector<string> t;
-//        string tk;
-//
-//        while (iss >> tk) {
-//            t.push_back(tk);
-//        }
-//
-//        bool bg = false;
-//        if (!t.empty() && t[0].front() == '&') {
-//            bg = true;
-//            t[0] = t[0].substr(1);
-//        }
-//
-//        cout << "prompt> ";
-//        for (const auto& tk : t) {
-//            cout << tk << " ";
-//        }
-//        cout << endl;
-//
-//        int rc = 1;
-//        int pd = 0;
-//        int dr = 300;
-//
-//        for (size_t i = 0; i < t.size(); ++i) {
-//            if (t[i] == "-n") {
-//                if (i + 1 < t.size()) {
-//                    rc = stoi(t[i + 1]);
-//                }
-//            }
-//            else if (t[i] == "-p") {
-//                if (i + 1 < t.size()) {
-//                    pd = stoi(t[i + 1]);
-//                }
-//            }
-//            else if (t[i] == "-d") {
-//                if (i + 1 < t.size()) {
-//                    dr = stoi(t[i + 1]);
-//                }
-//            }
-//        }
-//
-//        vector<string> ctk;
-//        for (size_t i = 0; i < t.size(); ++i) {
-//            if (t[i] != "-n" && t[i] != "-p" && t[i] != "-d") {
-//                ctk.push_back(t[i]);
-//            }
-//            else {
-//                break;
-//            }
-//        }
-//
-//        auto command_exec = [ctk, rc, pd, dr, &jq, &fg_sem, bg]() {
-//            if (!bg) {
-//                fg_sem.wait();
-//            }
-//
-//            const string& c = ctk[0];
-//            if (c == "echo") {
-//                Echo(ctk, rc, pd, dr, jq);
-//            }
-//            else if (c == "dummy") {
-//                Dummy(ctk, rc, pd, dr, jq);
-//            }
-//            else if (c == "gcd") {
-//                GCD(ctk, rc, pd, dr, jq);
-//            }
-//            else if (c == "prime") {
-//                Prime(ctk, rc, pd, dr, jq);
-//            }
-//            else if (c == "sum") {
-//                Sum(ctk, rc, pd, dr, jq);
-//            }
-//            else {
-//                cerr << "Unknown command: " << c << endl;
-//            }
-//
-//            if (!bg) {
-//                fg_sem.notify();
-//            }
-//            };
-//
-//        thread th(command_exec);
-//        if (bg) {
-//            jq.addJ(make_shared<J>(jq.tc++, false, line));
-//            th.detach();
-//        }
-//        else {
-//            th.join();
-//            fpc++;
-//        }
-//
-//        jq.display(fpc);
-//    }
-//}
-//
-//int main() {
-//    int fpc = 0;
-//    string fn = "commands.txt";
-//    JQ jq;
-//
-//    procFile(fn, fpc, jq);
-//
-//    return 0;
-//}
+// 2-2 2-3 완성
+// 2-3의 -m 구현 못함
+#include <iostream>
+#include <thread>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <iomanip>
+#include <Windows.h>
+#include <chrono>
+#include <stdlib.h>
+
+#define Y   500
+
+using namespace std;
+using namespace std::chrono;
+mutex mtx;
+
+class CommandQueue {
+private:
+    queue<vector<string>> commands;
+    mutex mtx;
+    condition_variable cv;
+    bool done = false;
+
+public:
+    void push(const vector<string>& command) {
+        {
+            lock_guard<mutex> lock(mtx);
+            commands.push(command);
+        }
+        cv.notify_one();
+    }
+
+    vector<string> pop() {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [&] { return !commands.empty() || done; });
+        if (commands.empty() && done) return {};
+        vector<string> command = commands.front();
+        commands.pop();
+        return command;
+    }
+
+    void setDone() {
+        {
+            lock_guard<mutex> lock(mtx);
+            done = true;
+        }
+        cv.notify_all();
+    }
+
+    bool isDone() {
+        lock_guard<mutex> lock(mtx);
+        return done && commands.empty();
+    }
+};
+
+void Echo(const vector<string>& t) {
+    for (size_t j = 1; j < t.size(); ++j) {
+        cout << t[j] << " ";
+    }
+    cout << endl;
+}
+
+void Dummy() {
+
+}
+
+void GCD(const vector<string>& t) {
+    if (t.size() != 3) {
+        cerr << "Invalid arguments for gcd command" << endl;
+        return;
+    }
+    int x = stoi(t[1]);
+    int y = stoi(t[2]);
+    while (y != 0) {
+        int temp = x % y;
+        x = y;
+        y = temp;
+    }
+    cout << x << endl;
+}
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
+void Prime(const vector<string>& t) {
+    if (t.size() != 2) {
+        cerr << "Invalid arguments for prime command" << endl;
+        return;
+    }
+    int x = stoi(t[1]);
+    int pc = 0;
+    for (int i = 2; i <= x; ++i) {
+        if (isPrime(i)) {
+            ++pc;
+        }
+    }
+    cout << pc << endl;
+}
+
+void Sum(const vector<string>& t) {
+    if (t.size() != 2) {
+        cerr << "Invalid arguments for sum command" << endl;
+        return;
+    }
+    int x = stoi(t[1]);
+    long long s = 0;
+    const int MOD = 1000000;
+    for (int i = 1; i <= x; ++i) {
+        s = (s + i) % MOD;
+    }
+    cout << s << endl;
+}
+
+void excSC(const vector<string>& t) {
+    const string& c = t[0];
+    if (c == "echo") {
+        Echo(t);
+    }
+    else if (c == "dummy") {
+        Dummy();
+    }
+    else if (c == "gcd") {
+        GCD(t);
+    }
+    else if (c == "prime") {
+        Prime(t);
+    }
+    else if (c == "sum") {
+        Sum(t);
+    }
+    else {
+        cerr << "Unknown command: " << c << endl;
+    }
+}
+
+void executeCommand(const vector<string>& t) {
+    int r = 1;
+    int p = 0;
+    int d = 300;
+    int y = 20;
+
+    for (size_t i = 0; i < t.size(); ++i) {
+        if (t[i] == "-n") {
+            if (i + 1 < t.size()) {
+                r = stoi(t[i + 1]);
+            }
+        }
+        else if (t[i] == "-p") {
+            if (i + 1 < t.size()) {
+                p = stoi(t[i + 1]);
+            }
+        }
+        else if (t[i] == "-d") {
+            if (i + 1 < t.size()) {
+                d = stoi(t[i + 1]);
+            }
+        }
+    }
+
+    vector<string> CT;
+    for (size_t i = 0; i < t.size(); ++i) {
+        if (t[i] != "-n" && t[i] != "-p" && t[i] != "-d") {
+            CT.push_back(t[i]);
+        }
+        else {
+            break;
+        }
+    }
+
+    int rp = r * p * y;
+    for (int count = 0; count < r; ++count) {
+        excSC(CT);
+
+        if (p > 0 && d > 0) {
+            Sleep(p * y);
+            if (rp >= d) {
+                cout << "실행시간 초과" << endl;
+                exit(1);
+            }
+        }
+        else if (p > 0) {
+            Sleep(p * y);
+        }
+    }
+}
+
+void Monitor(int fgCount, int bgCount, int doneFg, int doneBg) {
+    cout << endl;
+    cout << "Running: [" << fgCount << "F] [" << bgCount << "B]" << endl;
+    cout << "---------------------------" << endl;
+    cout << "DQ: P => ";
+    for (int i = 0; i < doneFg; i++) {
+        cout << "[" << i << "F] ";
+    }
+    for (int i = 0; i < doneBg; i++) {
+        cout << " [" << i << "B]";
+    }
+    cout << " (bottom / top)" << endl;
+    cout << "---------------------------" << endl;
+    cout << "WQ: []" << endl;
+    cout << endl;
+}
+
+void procFile(const string& filename, CommandQueue& fgQueue, CommandQueue& bgQueue, int& fgCount, int& bgCount, int& doneFg, int& doneBg) {
+    ifstream file(filename);
+    string line;
+    while (getline(file, line)) {
+        istringstream iss(line);
+        vector<string> t;
+        string tk;
+        while (iss >> tk) {
+            t.push_back(tk);
+        }
+        if (t.empty()) continue;
+        cout << "prompt> ";
+        for (const auto& tk : t) {
+            cout << tk << " ";
+        }
+        cout << endl;
+        if (t[0][0] == '&') {
+            t[0].erase(0, 1);  // Remove '&' character for BG commands
+            bgQueue.push(t);
+            bgCount++;
+        }
+        else {
+            fgQueue.push(t);
+            fgCount++;
+        }
+        Sleep(Y);
+    }
+
+    fgQueue.setDone();
+    bgQueue.setDone();
+}
+
+void proC(CommandQueue& queue, mutex& printMutex, int& fgCount, int& bgCount, int& doneFg, int& doneBg, bool isFG) {
+    while (true) {
+        vector<string> command = queue.pop();
+        if (command.empty()) break;
+        {
+            lock_guard<mutex> lock(printMutex);
+            executeCommand(command);
+        }
+        if (isFG) {
+            doneFg++;
+        }
+        else {
+            doneBg++;
+        }
+        lock_guard<mutex> lock(printMutex);
+        Monitor(fgCount, bgCount, doneFg, doneBg);
+    }
+}
+
+int main() {
+    string fn = "commands.txt";
+    CommandQueue fgQueue;
+    CommandQueue bgQueue;
+    mutex printMutex;
+    int fgCount = 0;
+    int bgCount = 0;
+    int doneFg = 0;
+    int doneBg = 0;
+
+    thread fgThread(proC, ref(fgQueue), ref(printMutex), ref(fgCount), ref(bgCount), ref(doneFg), ref(doneBg), true);
+    thread bgThread(proC, ref(bgQueue), ref(printMutex), ref(fgCount), ref(bgCount), ref(doneFg), ref(doneBg), false);
+
+    procFile(fn, fgQueue, bgQueue, fgCount, bgCount, doneFg, doneBg);
+
+    fgThread.join();
+    bgThread.join();
+
+    return 0;
+}
